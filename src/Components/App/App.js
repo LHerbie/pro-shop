90556001b205fbcfe78131fe8d354649cfc58e2d
Lara Herbst <laraherbst@Laras-MacBook-Air.local>
2023-10-04 15:05:28 +1100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import { Button, Container, Paper, Typography } from '@mui/material';
+import logo from './../../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Container maxWidth="sm" className="App">
+      <Paper>
+        <img src = {logo}className="App-logo" alt="logo" />
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App + Material-UI
+        </Typography>
+        <Button variant="contained" color="primary">
+          Primary Button
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary Button
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
